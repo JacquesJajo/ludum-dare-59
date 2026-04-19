956c622950d7264ad0b9df0f619e134526162a81
Jacques Yon <jacques2000.x3@gmail.com>
2026-04-19 15:23:23 +0100

    dialogue setup

--- scenes/actions/action.gd
+++ scenes/actions/action.gd
@@ -5,6 +5,7 @@ class_name Action
 const CRITICAL: int = 20
 
 @export var action_name: String
+@onready var dialogue_timer: Timer = $DialogueTimer
 
 var user: Character
 var target: Character
@@ -19,3 +20,18 @@ func do_action() -> void:
 
 func _receive_signal() -> void:
 	pass
+
+func show_message(message: String) -> void:
+	if Dialogic.current_timeline != null:
+		return
+	
+	var events: Array[String] = [message]
+	var timeline: DialogicTimeline = DialogicTimeline.new()
+	timeline.events = events
+	Dialogic.start(timeline)
+	
+	dialogue_timer.start()
+
+
+func _on_dialogue_timer_timeout() -> void:
+	Dialogic.end_timeline()
